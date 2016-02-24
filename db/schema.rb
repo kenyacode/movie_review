935a61dd09742727844a16260290000f4370b913
Joshua Munene <kenyadevelop@gmail.com>
2016-02-23 22:14:32 -0800

    Converted review ratings to stars. @avg_review does not work.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224025332) do
+ActiveRecord::Schema.define(version: 20160224030507) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160224025332) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
