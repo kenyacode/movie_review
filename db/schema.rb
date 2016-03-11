b67cdd5629f2d80fbfe45d9ef978f16f873f31ca
Joshua Munene <kenyadevelop@gmail.com>
2016-03-10 21:56:39 -0800

    removed rating from reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224030507) do
+ActiveRecord::Schema.define(version: 20160311055351) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20160224030507) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer  "rating"
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
