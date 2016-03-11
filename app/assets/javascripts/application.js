70d71997f3e2b9a1f82d7a1fed4d08a4306566f4
Joshua Munene <kenyadevelop@gmail.com>
2016-03-10 22:07:17 -0800

    removed rating files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery
-//= require jquery.raty
-//= require ratyrate
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
